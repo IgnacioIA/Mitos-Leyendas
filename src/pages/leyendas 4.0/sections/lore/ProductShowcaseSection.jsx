d683b837bc83bdb9738692b3f8b86314cbd78ea7
ignacioIA <ignaciocruz1@hotmail.com>
2026-05-16 23:26:50 -0300

    Fix: Se arregla composicion de imagen y texto en Lore

--- src/pages/leyendas 4.0/sections/lore/ProductShowcaseSection.jsx
+++ src/pages/leyendas 4.0/sections/lore/ProductShowcaseSection.jsx
@@ -3,23 +3,21 @@ import "./styles/product-showcase-section.css";
 import ShowcaseContent from "./components/ShowcaseContent";
 import ShowcaseImage from "./components/ShowcaseImage";
 import ShowcaseButton from "./components/ShowcaseButton";
-import ShowcaseFog from "./components/ShowcaseFog";
 
 function ProductShowcaseSection() {
 
   return (
     <section
-      id="next-section"
       className="product-showcase section"
     >
 
-      <ShowcaseFog />
 
       <div className="container">
 
         <div className="showcase-layout">
 
-          <div className="showcase-info">
+          <div className="showcase-info"
+          id="next-section">
             <ShowcaseContent />
             <ShowcaseButton />
           </div>
